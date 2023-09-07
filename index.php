f8515a8653631d8d95a03590c6636d7e9ea21b6b
Yogesh Manni <yogeshmanni786@gmail.com>
2023-09-07 10:51:05 -0700

    Updated index.php with Arrays to dynamically render list of books

--- index.php
+++ index.php
@@ -6,30 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Demo</title>
     <style>
-    body {
-        display: grid;
-        place-items: center;
-        height: 100vh;
-        font-family: sans-serif;
-        margin: 0;
-    }
+
     </style>
 </head>
 
 <body>
 
+    <h1> Recommended Books </h1>
     <?php 
-           $bookName = "Dark Matter";
-           $isRead = true;
+      $books = [
+        "Do Androids Dream of Electronic Sheeps",
+        "The Langoliers",
+        "Hail Mary",
+      ]
+          ?>
 
-           if($isRead)
-            $message = "You have read $bookName";
-           else
-            $message = "You have not read $bookName"
-        ?>
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php
+              foreach($books as $book)
+              {
+                echo "<li>$book</li>";
+              }
+            ?>
+    </ul>
 </body>
 
 </html>
